d5efe0dda05b5e90835b950723750cff14d0edba
Brayden Lilienthal <Lilienthalbrayden@gmail.com>
2025-07-10 23:59:22 -0500

    Stores each frame in memory

--- main.c
+++ main.c
@@ -27,24 +27,24 @@ int main(int argc, char* argv[])
 	struct timespec time;
 	struct timespec remaining_time;
 	time.tv_sec = 0;
-	time.tv_nsec = 100000000;
+	time.tv_nsec = 150000000;
 
 
 	//this is the current frame stored as a string
-	char currFrame[1000];
+	char frames[30][1000];
 	
 	//this stores the sizes of each animatiom, so we can just access it in a loop easier.
-	int animationSize;
+	int frameSize[30];
 
 
-	for (int i = 1; i <= atoi(argv[2]); i++)
+	for (int i = 0; i < atoi(argv[2]); i++)
 	{
 		
 
 		//current file name
 		char currentFile[200];
 
-		sprintf(currentFile, "%s%d.txt", argv[1], i);
+		sprintf(currentFile, "%s%d.txt", argv[1], i+1);
 
 
 		int animFile = open(currentFile, O_RDONLY);
@@ -53,24 +53,11 @@ int main(int argc, char* argv[])
 			exit(EXIT_FAILURE);
 		}
 
-		animationSize = read(animFile, currFrame, 1000);
-		
-		Nanosleep(time, remaining_time);
+		frameSize[i] = read(animFile, frames[i], 1000);
 
-		//clearing the terminal each time we go to display our frame
-		system("clear");
-
-		if (animationSize == -1)
+		if (frameSize[i] == -1)
 		{
 			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			//displaying frame to terminal
-			write(STDOUT_FILENO, currFrame, animationSize);
-			write(STDOUT_FILENO, "\n\n\n", 4);
-			fflush(stdout);
-
 		}
 
 		//closing file
@@ -78,14 +65,24 @@ int main(int argc, char* argv[])
 		{
 			exit(EXIT_FAILURE);
 		}
-		
+	}
+
+	for (int i = 0; i < atoi(argv[2]); i++)
+	{
+		//clearing the terminal each time we go to display our frame
+		system("clear");
+
+		//displaying frame to terminal
+		write(STDOUT_FILENO, frames[i], frameSize[i]);
+		write(STDOUT_FILENO, "\n\n\n", 4);
+		fflush(stdout);
+
 		//sleeping for set amount of time
-		
-		if (i == atoi(argv[2]))
+		Nanosleep(time, remaining_time);
+		if (i == atoi(argv[2])-1)
 		{
 			i = 0;
 		}
-		
 	}
 	
 }
